1788528d675cb1f8ab1ed73bd3e1e8c4d28d6bbd
Dacasa <david_4_5@hotmail.com>
2017-09-18 19:51:56 +0200

    prueba2

--- FlotaLocal/src/tablero/Partida.java
+++ FlotaLocal/src/tablero/Partida.java
@@ -73,12 +73,12 @@ public class Partida {
 		char ori = barcos[bar].getOrientacion();
 		int co = barcos[bar].getColumnaInicial();
 		int fi = barcos[bar].getFilaInicial();
-		
+		//PRUEBA PARA GIT.BORRAR
 		int f=0; int c=0;
 		
 		for(int i=0 ; i<tam ; i++){
 			mar[f][c] = -3;
-			if(ori==H)
+			if(ori=='H')
 				c++;
 			else
 				f++;
